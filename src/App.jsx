7b5a69246c6167fed6d76c4ff72a92a1699d3954
User <user@example.com>
2026-02-24 16:32:26 -0800

    added app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CircleDrawing from './components/CircleDrawing'
+
+
+function App() {
+  return (
+      <div className="App">
+        <CircleDrawing />
+      </div>
+  );
+}
+
+
+export default App;
